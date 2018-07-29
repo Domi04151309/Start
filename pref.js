0747cb470256b160c3e1da0d64b0300b72ef04c3
Domi <domi04151309business@gmail.com>
2018-07-29 18:16:37 +0200

    Added image cookie

--- pref.js
+++ pref.js
@@ -9,7 +9,21 @@ function getName(){
     document.getElementById('name').innerHTML = "Your name is " + name;
   }
 }
+function setBg(){
+  var background = prompt("Please enter an image URL");
+  setCookie("image", background, 365);
+	document.getElementById('image').innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
+}
+function getBg(){
+  var background = getCookie("image");
+  if (background != "") {
+    document.getElementById('image').innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
+  }
+}
 function deleteCookies(){
   removeCookie("name");
+  removeCookie("image");
+  document.getElementById('name').innerHTML = 'No name has been set';
+  document.getElementById('image').innerHTML = 'No custom background has been set';
   alert("Deleted all cookies")
 }
