349ec3cf8cc6fd56e39c38ce71779f25efb1b797
Domi <domi04151309business@gmail.com>
2018-07-29 18:53:00 +0200

    Fixed bug

--- pref.js
+++ pref.js
@@ -1,7 +1,11 @@
 function setName(){
   var name = prompt("Please enter your name");
   setCookie("name", name, 365);
-	document.getElementById('name').innerHTML = "Your name is " + name;
+	if (name != "") {
+    document.getElementById('name').innerHTML = "Your name is " + name;
+  }else {
+    document.getElementById('name').innerHTML = 'No name has been set';
+  }
 }
 function getName(){
   var name = getCookie("name");
@@ -12,7 +16,11 @@ function getName(){
 function setBg(){
   var background = prompt("Please enter an image URL");
   setCookie("image", background, 365);
-	document.getElementById('image').innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
+  if (background != "") {
+    document.getElementById('image').innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
+  }else {
+    document.getElementById('image').innerHTML = 'No custom background has been set';
+  }
 }
 function getBg(){
   var background = getCookie("image");
