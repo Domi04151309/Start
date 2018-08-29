240d0a5dc689852bc52b2f97cc8a280d70d314a3
Domi <domi04151309business@gmail.com>
2018-08-29 14:40:40 +0200

    Added input fields

--- pref.js
+++ pref.js
@@ -1,57 +1,61 @@
 function setName(){
-  var name = prompt("Please enter your name");
-  setCookie("name", name, 365);
-	if (name != "") {
-    document.getElementById('name').innerHTML = "Your name is " + name;
+  var nameInput = document.getElementById('nameInput');
+  var name = nameInput.value;
+  nameInput.value = '';
+  setCookie('name', name, 365);
+	if (name != '') {
+    document.getElementById('name').innerHTML = 'Your name is ' + name;
   }else {
     document.getElementById('name').innerHTML = 'No name has been set';
   }
 }
 function getName(){
-  var name = getCookie("name");
-  if (name != "") {
-    document.getElementById('name').innerHTML = "Your name is " + name;
+  var name = getCookie('name');
+  if (name != '') {
+    document.getElementById('name').innerHTML = 'Your name is ' + name;
   }
 }
 function setBg(){
-  var background = prompt("Please enter an image URL");
-  setCookie("image", background, 365);
-  if (background != "") {
+  var bgInput = document.getElementById('bgInput');
+  var background = bgInput.value;
+  bgInput.value = '';
+  setCookie('image', background, 365);
+  if (background != '') {
     document.getElementById('image').innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
   }else {
     document.getElementById('image').innerHTML = 'No custom background has been set';
   }
 }
 function getBg(){
-  var background = getCookie("image");
-  if (background != "") {
+  var background = getCookie('image');
+  if (background != '') {
     document.getElementById('image').innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
   }
 }
 function setMode(){
-  var mode = getCookie("mode");
-  if (mode == "") {
-    setCookie("mode", "dark", 365);
+  var mode = getCookie('mode');
+  if (mode == '') {
+    setCookie('mode', 'dark', 365);
     document.getElementById('mode').innerHTML = 'Text color: black';
   }else {
-    setCookie("mode", "", 365);
+    setCookie('mode', '', 365);
     document.getElementById('mode').innerHTML = 'Text color: white';
   }
 }
 function getMode(){
-  var mode = getCookie("mode");
-  if (mode == "dark") {
+  var mode = getCookie('mode');
+  if (mode == 'dark') {
     document.getElementById('mode').innerHTML = 'Text color: black';
   }else {
     document.getElementById('mode').innerHTML = 'Text color: white';
   }
 }
 function deleteCookies(){
-  removeCookie("name");
-  removeCookie("image");
-  removeCookie("mode");
+  removeCookie('name');
+  removeCookie('image');
+  removeCookie('mode');
   document.getElementById('name').innerHTML = 'No name has been set';
   document.getElementById('image').innerHTML = 'No custom background has been set';
   document.getElementById('mode').innerHTML = 'Text color: white';
-  alert("Deleted all cookies")
+  alert('Deleted all cookies')
 }
