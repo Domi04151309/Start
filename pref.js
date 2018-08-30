c7124c2318ae7bc18b732a051ed5e50afdf2fe0e
Domi <domi04151309business@gmail.com>
2018-08-30 18:56:55 +0200

    Changed pref script

--- pref.js
+++ pref.js
@@ -1,32 +1,52 @@
+var log = document.getElementById('log');
+var nameTxt = document.getElementById('name');
+var nameInput = document.getElementById('nameInput');
+var bgTxt = document.getElementById('image');
+var colorTxt = document.getElementById('mode');
+var bgInput = document.getElementById('bgInput');
+var bgFile = document.getElementById('bgFile');
 function setName(){
-  var nameInput = document.getElementById('nameInput');
   var name = nameInput.value;
-  nameInput.value = '';
-  setCookie('name', name, 365);
 	if (name != '') {
-    document.getElementById('name').innerHTML = 'Your name is ' + name;
+    nameInput.value = '';
+    setCookie('name', name, 365);
+    nameTxt.innerHTML = 'Your name is ' + name;
+    logTxt('Changed name');
   }else {
-    document.getElementById('name').innerHTML = 'No name has been set';
+    error(nameTxt);
   }
+}
+function delName(){
+  removeCookie('name');
+  nameTxt.innerHTML = 'No name has been set';
+  logTxt('Deleted name');
 }
 function getName(){
   var name = getCookie('name');
   if (name != '') {
-    document.getElementById('name').innerHTML = 'Your name is ' + name;
+    nameTxt.innerHTML = 'Your name is ' + name;
   }
 }
 function setBg(){
-  var bgInput = document.getElementById('bgInput');
   var background = bgInput.value;
-  bgInput.value = '';
-  setCookie('image', background, 365);
-  if (background != '' && background.length <= 100) {
-    document.getElementById('image').innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
-  }else if (background != '' && background.length > 100){
-    document.getElementById('image').innerHTML = 'The image URL is too long to display<br><br><img src="' + background + '">';
-  }else {
-    document.getElementById('image').innerHTML = 'No custom background has been set';
+  if(background != ''){
+    bgInput.value = '';
+    setCookie('image', background, 365);
+    if (background.length <= 100) {
+      bgTxt.innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
+      logTxt('Changed background image');
+    }else if (background.length > 100){
+      bgTxt.innerHTML = 'The image URL is too long to display<br><br><img src="' + background + '">';
+      logTxt('Changed background image');
+    }else {
+      error(bgTxt);
+    }
   }
+}
+function delBg(){
+  removeCookie('image');
+  bgTxt.innerHTML = 'No custom background has been set';
+  logTxt('Reset background');
 }
 function getBg(){
   var background = getCookie('image');
@@ -34,9 +54,9 @@ function getBg(){
     background = decodeURIComponent(background);
   }
   if (background != '' && background.length <= 100) {
-    document.getElementById('image').innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
+    bgTxt.innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
   }else if (background != '' && background.length > 100){
-    document.getElementById('image').innerHTML = 'The image URL is too long to display<br><br><img src="' + background + '">';
+    bgTxt.innerHTML = 'The image URL is too long to display<br><br><img src="' + background + '">';
   }
 }
 function getBase64(file){
@@ -46,50 +66,58 @@ function getBase64(file){
     var background = reader.result;
     var bgCookie = encodeURIComponent(background);
     setCookie('image', bgCookie, 365);
-  if (background != '' && background.length <= 100) {
-    document.getElementById('image').innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
-  }else if (background != '' && background.length > 100){
-    document.getElementById('image').innerHTML = 'The image URL is too long to display<br><br><img src="' + background + '">';
-  }else {
-    document.getElementById('image').innerHTML = 'No custom background has been set';
-  }
+    if (background != '' && background.length <= 100){
+      bgTxt.innerHTML = 'The image URL is <a href="' + background + '">' + background + '</a><br><br><img src="' + background + '">';
+    }else if (background != '' && background.length > 100){
+      bgTxt.innerHTML = 'The image URL is too long to display<br><br><img src="' + background + '">';
+    }else{
+      bgTxt.innerHTML = 'No custom background has been set';
+    }
   };
   reader.onerror = function (error) {
-    console.log('Error: ', error);
+    logTxt('Error: ', error);
   };
 }
 function uploadBg(){
-  var fileInput = document.getElementById('bgFile');
-  var files = fileInput.files;
+  alert('Experimental feature!');
+  var files = bgFile.files;
   if (files.length > 0) {
     var background = getBase64(files[0]);
-    fileInput.value = '';
+    bgFile.value = '';
   }
 }
 function setMode(){
   var mode = getCookie('mode');
   if (mode == '') {
     setCookie('mode', 'dark', 365);
-    document.getElementById('mode').innerHTML = 'Text color: black';
+    colorTxt.innerHTML = 'Text color: black';
   }else {
     setCookie('mode', '', 365);
-    document.getElementById('mode').innerHTML = 'Text color: white';
+    colorTxt.innerHTML = 'Text color: white';
   }
+  logTxt('Changed text color');
 }
 function getMode(){
   var mode = getCookie('mode');
   if (mode == 'dark') {
-    document.getElementById('mode').innerHTML = 'Text color: black';
+    colorTxt.innerHTML = 'Text color: black';
   }else {
-    document.getElementById('mode').innerHTML = 'Text color: white';
+    colorTxt.innerHTML = 'Text color: white';
   }
 }
 function deleteCookies(){
   removeCookie('name');
   removeCookie('image');
   removeCookie('mode');
-  document.getElementById('name').innerHTML = 'No name has been set';
-  document.getElementById('image').innerHTML = 'No custom background has been set';
-  document.getElementById('mode').innerHTML = 'Text color: white';
-  alert('Deleted all cookies')
+  nameTxt.innerHTML = 'No name has been set';
+  bgTxt.innerHTML = 'No custom background has been set';
+  colorTxt.innerHTML = 'Text color: white';
+  logTxt('Deleted all cookies');
+}
+function logTxt(param){
+  log.innerHTML = 'Last Action: ' + param;
+}
+function error(param){
+  logTxt('An unexpected error occured');
+  param.innerHTML = 'An unexpected error occured';
 }
