67b1d9709e449a63679b7a7318d22dc3d2bdfa09
Domi <domi04151309business@gmail.com>
2019-06-21 17:35:33 +0200

    Add service worker

---
+++ sw.js
@@ -0,0 +1,75 @@
+---
+# Service worker for Jekyll
+---
+var CACHE_NAME = 'start-cache-{{ site.time | date: "%Y-%m-%d-%H:%M" }}';
+var urlsToCache = [
+  './',
+  './pref',
+  './css/main.css',
+  './css/pref.css',
+  './js/pref.js',
+  './js/script.js',
+  './images/back.svg',
+  './images/bg.jpg',
+  './images/gear_black.svg',
+  './images/gear.svg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then(
+        function(response) {
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        }
+      );
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
